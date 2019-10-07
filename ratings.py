779d22dbcd43f80ba869170e9780d84c13a5bb3b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 12:40:36 -0700

    First commit with pseudocode

---
+++ ratings.py
@@ -0,0 +1,14 @@
+"""Restaurant rating lister."""
+"""
+Unzip a file
+Iterate over lines
+split at : ==> list of 2 items
+tuple the list 
+dict the tuple ==> dictionary with key-value pairs
+Use sorted() on the dictionary, save to a variable ==> alphabeized list of keys 
+Iterate over the dictionary
+    Unpacking and use item to index through dict
+    print items (key + value)
+"""
+
+# put your code here
