a34b4760ace7e9a0e95e5852d9f0e0a98a26f6ce
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-07 17:33:51 -0700

    Added user input for restaurant ratings and integer error catching.

--- ratings.py
+++ ratings.py
@@ -23,9 +23,35 @@ def print_restaurant_ratings(path):
         restaurant_and_rating = line.rstrip().split(':')
         restaurant_ratings[restaurant_and_rating[0]] = restaurant_and_rating[1]
 
+    add_more = "Yes"
+
+    while add_more == "Yes":
+        new_restaurant = input("Add a new restaurant name: ")
+        new_rating = input("Please type an integer rating (1-5): ")
+
+        while not new_rating.isnumeric() or int(new_rating) < 1 or 5 < int(new_rating):
+            new_rating = input("Please enter a valid integer rating between 1 and 5: ")
+        
+        restaurant_ratings[new_restaurant] = new_rating
+        add_more = input("Would you like to add another restauarant? Yes / No ").title()
+
     sorted_restaurants = sorted(restaurant_ratings)
 
     for restaurant in sorted_restaurants:
         print(f"{restaurant}: {restaurant_ratings[restaurant]}")
     
     file.close()
+
+"""PSEUDOCODE
+open file, get restaurant ratings from file
+
+loop until don't add more:
+    input for restaurant name
+    input for restaurant score (rating)
+    do you want to add another?
+
+store restaurant + rating in dic
+then sort
+then iterate + print
+"""
+
